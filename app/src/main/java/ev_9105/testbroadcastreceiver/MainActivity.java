a3dab2e9056ca8b6148ba096355549d60257693d
Jianhui Zhu <jianhuizhu1987@gmail.com>
2015-05-02 17:24:33 -0400

    add custom receiver

--- app/src/main/java/ev_9105/testbroadcastreceiver/MainActivity.java
+++ app/src/main/java/ev_9105/testbroadcastreceiver/MainActivity.java
@@ -1,19 +1,37 @@
 package ev_9105.testbroadcastreceiver;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
-
+    private IntentFilter intentFilter;
+    private InfoReceiver infoReceiver;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        intentFilter = new IntentFilter();
+        //Add what kind of broadcast you want to listen
+        intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
+        infoReceiver=new InfoReceiver();
+        registerReceiver(infoReceiver,intentFilter);
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(infoReceiver);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -33,7 +51,18 @@ public class MainActivity extends ActionBarActivity {
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
+    }
+    public class InfoReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo networkInfo=connectivityManager.getActiveNetworkInfo();
+            if(networkInfo!=null&&networkInfo.isAvailable()){
+                Toast.makeText(context,"network is available",Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(context,"network is unavailable",Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
---
+++ app/src/main/java/ev_9105/testbroadcastreceiver/MyBroadcastReceiver.java
@@ -0,0 +1,15 @@
+package ev_9105.testbroadcastreceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by fishsb19 on 5/2/2015.
+ */
+public class MyBroadcastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
